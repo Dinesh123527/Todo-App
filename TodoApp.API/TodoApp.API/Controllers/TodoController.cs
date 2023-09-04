46265a41dbf2c6ac819dc7aa3e62a282295fefef
Dinesh123527 <varmadinesh249@gmail.com>
2023-09-04 07:19:53 +0530

    first commit

---
+++ TodoApp.API/TodoApp.API/Controllers/TodoController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.API.Data;
+using TodoApp.API.Models;
+
+namespace TodoApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoDbContext _todoDbContext;
+
+        public TodoController(TodoDbContext todoDbContext)
+        {
+            _todoDbContext = todoDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTodos()
+        {
+            var todos = await _todoDbContext.Todos
+                .Where(x => x.IsDeleted == false)
+                .OrderByDescending(x => x.CreatedDate)
+                .ToListAsync();
+
+            return Ok(todos);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddTodo(Todo todo)
+        {
+            todo.Id = Guid.NewGuid();
+            _todoDbContext.Todos.Add(todo);
+            await _todoDbContext.SaveChangesAsync();
+            return Ok(todo);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateTodo([FromRoute] Guid id, Todo todoUpdateRequest)
+        {
+            var todo = await _todoDbContext.Todos.FindAsync(id);
+
+            if (todo == null)
+                return NotFound();
+
+            todo.IsCompleted = todoUpdateRequest.IsCompleted;
+            todo.CompletedDate = DateTime.Now;
+
+            await _todoDbContext.SaveChangesAsync();
+
+            return Ok(todo);
+        }
+
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteTodo([FromRoute] Guid id)
+        {
+            var todo = await _todoDbContext.Todos.FindAsync(id);
+
+            if (todo == null)
+                return NotFound();
+
+            todo.IsDeleted = true;
+            todo.DeletedDate = DateTime.Now;
+
+            await _todoDbContext.SaveChangesAsync();
+
+            return Ok(todo);
+        }
+
+        [HttpGet]
+        [Route("api/deleted-todos")]
+        public async Task<IEnumerable<Todo>> GetDeletedTodos()
+        {
+            var deletedTodos = await _todoDbContext.Todos.Where(t => t.IsDeleted).ToListAsync();
+            return deletedTodos;
+        }
+
+        [HttpDelete]
+        [Route("api/deleted-todos/clear")]
+        public async Task<IActionResult> ClearDeletedTodos()
+        {
+            var deletedTodos = await _todoDbContext.Todos.Where(t => t.IsDeleted).ToListAsync();
+            _todoDbContext.Todos.RemoveRange(deletedTodos);
+            await _todoDbContext.SaveChangesAsync();
+            return Ok(deletedTodos);
+
+        }
+
+    }
+}
---
+++ TodoApp.API/TodoApp.API/Migrations/20230901101917_Added delete property fields.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApp.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addeddeletepropertyfields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "isCompleted",
+                table: "Todos",
+                newName: "IsCompleted");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedDate",
+                table: "Todos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Todos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedDate",
+                table: "Todos");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Todos");
+
+            migrationBuilder.RenameColumn(
+                name: "IsCompleted",
+                table: "Todos",
+                newName: "isCompleted");
+        }
+    }
+}
